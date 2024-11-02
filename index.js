11318f6cf3f214d83fc827def8b342f782c72b1e
laibahussain <laibah42@gmail.com>
2024-11-02 14:40:25 +0500

    basic callback

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ function goodbye(){
 console.log("Goodbye")
 }
 
-// callback hell is a situation in asynchronous programing when multiple nested callbacks are used due to which code readability and maintanability suffers. to avoid it promises orasync/await can be used 
+// callback hell is a situation in asynchronous programing when multiple nested callbacks are used due to which code readability and maintanability suffers. to avoid it promises or async/await can be used 
