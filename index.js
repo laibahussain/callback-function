de6e7ef2492d4747789bd96e8fbc8238b57caf9b
laibahussain <laibah42@gmail.com>
2024-11-02 14:39:33 +0500

    basic callback

---
+++ index.js
@@ -0,0 +1,14 @@
+// callback function is a function(goodbye) that is passed as an argument to another function(hello)
+
+hello(goodbye)
+
+function hello(callback){
+console.log("hello")
+callback()
+}
+
+function goodbye(){
+console.log("Goodbye")
+}
+
+// callback hell is a situation in asynchronous programing when multiple nested callbacks are used due to which code readability and maintanability suffers. to avoid it promises orasync/await can be used 
